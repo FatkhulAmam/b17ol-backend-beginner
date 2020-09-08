39562709500be2c9da3deae88d62a038e698d9e9
Ahmad Amam <ahmadfatkhul.amam@gmail.com>
2020-09-09 00:51:08 +0700

    make POST methode to display in the /profile at respons

--- index.js
+++ index.js
@@ -6,18 +6,26 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//data//
 app.get('/data', (req, res)=>{
     res.sendFile('index.html', {root: path.join(__dirname, './file')})
 })
-
 app.get('/home', (req, res)=>{
     res.send("welcome to my first backend!!")
 })
 
 app.post('/data', (req, res)=> {
     console.log(JSON.stringify(req.body))
-    res.end(JSON.stringify(req.body))
 });
+
+//profile//
+app.get('/profile', (req, res)=>{
+    res.sendFile('profile.html', {root: path.join(__dirname, './file')})
+})
+app.post('/profile', (req, res)=>{
+    res.send(JSON.stringify(req.body))
+})
+    
 
 app.listen(2323,()=>{
     console.log("aplication running in port 2323")
