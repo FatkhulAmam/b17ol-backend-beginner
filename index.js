7d13035a8daa4800eba0ac137fc863d5cb22aae5
Ahmad Amam <ahmadfatkhul.amam@gmail.com>
2020-09-08 23:35:27 +0700

    make method POST to display the data in the console

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const path = require('path')
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/data', (req, res)=>{
+    res.sendFile('index.html', {root: path.join(__dirname, './file')})
+})
 
 app.get('/home', (req, res)=>{
     res.send("welcome to my first backend!!")
 })
+
+app.post('/data', (req, res)=> {
+    console.log(JSON.stringify(req.body))
+    res.end(JSON.stringify(req.body))
+});
 
 app.listen(2323,()=>{
     console.log("aplication running in port 2323")
