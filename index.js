0ad20f03d231adcee450713356cf3c5c6df14960
Ahmad Amam <ahmadfatkhul.amam@gmail.com>
2020-09-09 17:14:39 +0700

    post path /profile untuk menampilkan data yang dikirim ke respons

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const path = require('path')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended : false}));
 
-//data//
-app.get('/data', (req, res)=>{
-    res.sendFile('index.html', {root: path.join(__dirname, './file')})
-})
 app.get('/home', (req, res)=>{
     res.send("welcome to my first backend!!")
 })
 
-app.post('/data', (req, res)=> {
-    console.log(JSON.stringify(req.body))
-});
+app.post('/data', (req, res) => {
+    console.log(req.body)
+    res.sendStatus(200)
+})
 
-//profile//
-app.get('/profile', (req, res)=>{
-    res.sendFile('profile.html', {root: path.join(__dirname, './file')})
-})
 app.post('/profile', (req, res)=>{
-    res.send(JSON.stringify(req.body))
+    res.send(req.body)
 })
-    
 
 app.listen(2323,()=>{
     console.log("aplication running in port 2323")
